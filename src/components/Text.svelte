d7b8bb7f8fa043b49e4afb855c76737b4c5e4389
Rebecca Hjörvar <rebecca.hjorvarroutlook.com>
2021-11-02 14:54:32 +0100

    added text area

---
+++ src/components/Text.svelte
@@ -0,0 +1,18 @@
+<script>
+    export let inputText = 'This is an input text'
+</script>
+
+<div class="container">
+    <div>
+        <p>{inputText}</p>
+    </div>
+</div>
+
+<style>
+    .container{
+        display: flex;
+        justify-content: center;
+        margin: 1em auto;
+        font-family: sans-serif;
+    }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,8 +1,12 @@
 <script>
     import Nav from '../components/Nav.svelte'
+    import Text from '../components/Text.svelte'
+    
 </script>
 
 
 <Nav></Nav>
+
+<Text/>
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
