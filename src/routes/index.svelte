633a2d3b2857fa85249046c6849c2d157c0c9142
Rebecca Hjörvar <rebecca.hjorvarroutlook.com>
2021-11-02 15:35:44 +0100

    text with button that hides the div when clicked

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,12 +1,14 @@
 <script>
     import Nav from '../components/Nav.svelte'
     import Text from '../components/Text.svelte'
-    
+    import TextWithBtn from '../components/TextWithBtn.svelte'
 </script>
 
 
 <Nav></Nav>
 
 <Text/>
+
+<TextWithBtn/>
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
