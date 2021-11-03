e9d96ee2de1ce5d2963401f00479de6544dbfb51
ItsSteph <70627352+ItsSteph@users.noreply.github.com>
2021-11-03 13:59:32 +0100

    fixed colors

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -7,20 +7,30 @@
     import ImgLeft from '../components/ImgLeft.svelte'
     import ImgRight from '../components/ImgRight.svelte'
 
-let btn1;
-$: btn1;
-
+let btn1, color1, color2;
+$: btn1, color1, color2;
 </script>
 
 
-<input type="text" placeholder="btn1 text" bind:value={btn1}>
+<input type="text" placeholder="btn1 text" bind:value={btn1}><br>
+
+
+<label>Choose a background color for the first section: 
+    <input type="color" placeholder="color1" bind:value={color1}></label>
+<label>Choose a background color for the second section:
+    <input type="color" placeholder="color1" bind:value={color2}> </label>
 
 <Nav {btn1}></Nav>
 <img src="" alt=""> /* Här ska avataren vara */
 <h1>Title</h1>
 <Text></Text>
+<div style="background-color: {color1};">
 <ImgRight></ImgRight>
+</div>
+<div style="background-color: {color2};">
 <ImgLeft></ImgLeft>
+</div>
 <Carousel></Carousel>
 <TextWithBtn></TextWithBtn>
 <ContactForm></ContactForm>
+
